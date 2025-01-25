e5c2aae10692bbd8eff0f911d83d5fa5a060364f
Flavio Silva <flavioprogrammer@gmail.com>
2025-01-25 21:02:36 +0000

    desafio novato

--- xadrez.c
+++ xadrez.c
@@ -16,6 +16,58 @@ int main() {
 
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    const int quantidadeDeCasasBispo = 5;
+    const int quantidadeDeCasasTorre = 5;
+    const int quantidadeDeCasasRainha = 8;
+
+    int opcao;
+    do{
+        printf("\n");
+        printf("Escolha uma opção\n");
+        printf("1 - [Bispo]  5 casas direita, cima\n");
+        printf("2 - [Torre]  5 casas para a direita\n");
+        printf("3 - [Rainha] 8 casas para a esquerda\n");
+        printf("4 - Sair\n");
+        scanf("%d",&opcao);
+
+        switch (opcao)
+        {
+        case 1:
+            printf("[Bispo]\n");
+            for (int i = 0; i < quantidadeDeCasasBispo; i++) {
+                for (int j = quantidadeDeCasasBispo; j > i; j--)
+                {
+                    printf(" ");
+                }
+                
+                printf("Direita,cima\n");
+            }
+            break;
+        case 2:
+            printf("[Torre]\n");
+            for (int i = 0; i < quantidadeDeCasasTorre; i++){
+                printf("Direita\n");
+            }
+            
+            break;
+        case 3:
+            printf("[Rainha]\n");
+            for(int i = 0; i < quantidadeDeCasasRainha; i++){   
+                printf("Esquerda\n");
+            }
+            
+            break;
+        case 4:
+            printf("Saindo!\n");
+            break;            
+        default:
+            printf("Opcao invalida. tente novamente.\n");
+            break;
+        }
+
+    }while(opcao != 4);
+
+
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
